f498694696e5de6c611557e838a26c354520e6e1
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-03-28 12:11:12 +0800

    2.3.5 Signed-off-by: HY-ZhengWei <HY.ZhengWei@qq.com>

---
+++ Common.003.Win.Database.bat
@@ -0,0 +1,14 @@
+
+
+cd .\bin
+
+rd /s/q .\org\hy\common\configfile\junit
+rd /s/q .\org\hy\common\db\junit
+
+
+jar cvfm hy.common.db.jar MANIFEST.MF LICENSE org
+
+copy hy.common.db.jar ..
+del /q hy.common.db.jar
+cd ..
+
