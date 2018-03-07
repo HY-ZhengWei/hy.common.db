e845f268197c41864e5fb42b09dcd666bfecb6ce
HY-ZhengWei <hy.zhengwei@qq.com>
2018-03-07 12:03:48 +0800

    2.3.10

--- Common.003.Win.Database.bat
+++ Common.003.Win.Database.bat
@@ -2,9 +2,9 @@
 
 cd .\bin
 
+
 rd /s/q .\org\hy\common\configfile\junit
 rd /s/q .\org\hy\common\db\junit
-
 
 jar cvfm hy.common.db.jar MANIFEST.MF META-INF org
 
@@ -12,3 +12,12 @@ copy hy.common.db.jar ..
 del /q hy.common.db.jar
 cd ..
 
+
+
+
+
+cd .\src
+jar cvfm hy.common.db-sources.jar MANIFEST.MF META-INF org 
+copy hy.common.db-sources.jar ..
+del /q hy.common.db-sources.jar
+cd ..
--- Common.004.Mac.Database.sh
+++ Common.004.Mac.Database.sh
@@ -2,9 +2,9 @@
 
 cd ./bin
 
+
 rm -R ./org/hy/common/configfile/junit
 rm -R ./org/hy/common/db/junit
-
 
 jar cvfm hy.common.db.jar MANIFEST.MF META-INF org
 
@@ -12,3 +12,12 @@ cp hy.common.db.jar ..
 rm hy.common.db.jar
 cd ..
 
+
+
+
+
+cd ./src
+jar cvfm hy.common.db-sources.jar MANIFEST.MF META-INF org 
+cp hy.common.db-sources.jar ..
+rm hy.common.db-sources.jar
+cd ..
