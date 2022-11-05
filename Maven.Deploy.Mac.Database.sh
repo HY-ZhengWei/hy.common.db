c3571122287dd789baab92e833898d80c798c6b8
ZhengWei <HY.ZhengWei@qq.com>
2022-11-05 09:51:54 +0800

    2.5.8
    修改：POM配置引用最新的file包

--- Maven.Deploy.Mac.Database.sh
+++ Maven.Deploy.Mac.Database.sh
@@ -3,6 +3,5 @@
 mvn install:install-file -Dfile=hy.common.db.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.db/pom.xml
 mvn install:install-file -Dfile=hy.common.db-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.db/pom.xml
 
-
 mvn deploy:deploy-file   -Dfile=hy.common.db.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.db/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
 mvn deploy:deploy-file   -Dfile=hy.common.db-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.db/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:8081/repository/thirdparty
